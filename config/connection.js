8db999d1793b910ae0a6221bdbf9bc29e1f28a6d
Taylor Caton <TaylorCaton@hgbc.org>
2017-08-29 11:58:09 -0400

    added jaws db to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "hotdogs_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "hotdogs_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
